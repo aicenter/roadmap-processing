dca04adae7dba8aebfdef6b9eae40e844df615b0
Martin Korytak <korytmar@fel.cvut.cz>
2017-09-13 17:38:57 +0200

    fixed ids

--- roadmaptools/export_nodes_and_id_maker.py
+++ roadmaptools/export_nodes_and_id_maker.py
@@ -60,11 +60,11 @@ def get_node_id(node_id):  # return String
     lon = int(node_id[0] * 10 ** 6)
     lat = int(node_id[1] * 10 ** 6)
     if lon < 0 and lat < 0:
-        return "1" + str(lon) + str(lat)
+        return "1" + str(lon)[1:] + str(lat)[1:]
     elif lon < 0 and lat >= 0:
-        return "2" + str(lon) + str(lat)
+        return "2" + str(lon)[1:] + str(lat)
     elif lon >= 0 and lat < 0:
-        return "3" + str(lon) + str(lat)
+        return "3" + str(lon) + str(lat)[1:]
     else:
         return str(lon) + str(lat)
 
