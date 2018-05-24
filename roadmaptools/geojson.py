00d2215de77a05708af92774f9fff771e4bf5075
David Fiedler <david.fiedler@agents.fel.cvut.cz>
2018-05-24 10:20:43 +0200

    merge_linestrings function in geojson fixed

--- roadmaptools/geojson.py
+++ roadmaptools/geojson.py
@@ -5,6 +5,6 @@ from geojson import LineString, MultiLineString
 def merge_linestrings(linestrings: Iterable[LineString]) -> MultiLineString:
 	linestrings_coords = []
 	for linestring in linestrings:
-		linestrings_coords.append(linestring["coordinates"])
+		linestrings_coords.append(linestring["geometry"]["coordinates"])
 
 	return MultiLineString(linestrings_coords)
