125855a0599f816644491d408334117719bf2c22
Martin Korytak <korytmar@fel.cvut.cz>
2018-07-30 15:27:30 +0200

    optimizing loading and processing csv

--- prune_gpx.py
+++ prune_gpx.py
@@ -53,6 +53,13 @@ def cut_gpx(in_file, out_file):
 #             traces_all.append(_load_traces_from_csv(abs_filename))
 #     return traces_all
 
+def sort_one_by_one(d, col1, col2):
+    """
+    Equivalent to pd.sort_values(by=[col1, col2]), but faster.
+    """
+    d.sort_values(by=[col2], inplace=True)
+    d.sort_values(by=[col1], kind='mergesort', inplace=True)
+
 import pandas as pd
 import numpy as np
 import roadmaptools
@@ -62,7 +69,7 @@ len_ids = set()
 pd.set_option('display.max_columns', None)
 column_names = ['id_record', 'id_car', 'status', 'lat', 'lon', 'time']
 # data_types = [str,int,str,float,float,str]
-arr = np.empty(shape=(len(os.listdir('/home/martin/MOBILITY/data/traces')),),dtype=object)
+# arr = np.empty(shape=(len(os.listdir('/home/martin/MOBILITY/data/traces')),),dtype=object)
 for idx,filename in tqdm(enumerate(os.listdir('/home/martin/MOBILITY/data/traces'))):
     abs_filename = os.path.join('/home/martin/MOBILITY/data/traces', filename)
     filename_parts = abs_filename.split(sep='.')
@@ -71,15 +78,20 @@ for idx,filename in tqdm(enumerate(os.listdir('/home/martin/MOBILITY/data/traces
     df['id_car'] = pd.to_numeric(file_extension + df['id_car'].astype(str))
     # print(df.head())
     # print(df.dtypes)
-    filtered = df[(df.iloc[:, 2] != "STREETPICKUP")].loc[:, ['id_car','lat', 'lon', 'time']].sort_values(by=['id_car','time'],ascending=True)
+    # q = sort_one_by_one(df,'id_car','time')
+    # print(q.head())
+    filtered = df[(df.iloc[:, 2] != "STREETPICKUP")].loc[:, ['id_car','lat', 'lon', 'time']]
+    filtered.sort_values(by='time',ascending=True,inplace=True)
+    filtered.sort_values(by='id_car',kind='mergesort', ascending=True, inplace=True)
     # print(filtered)
-    # ls.append(filtered)
-    arr[idx] = filtered
+    ls.append(filtered)
+    # arr[idx] = filtered
 
-df = pd.concat(arr,ignore_index=True)
+df = pd.concat(ls,ignore_index=True)
+# # exit(0)
 # print(df)
-sorted_df = df.sort_values(by=['id_car','time'],ascending=True)
-# print(sorted_df)
+# df.sort_values(by=['id_car','time'],ascending=True,inplace=True)
+# print(df)
 # for idx, row in df.iterrows():
 #     print(idx)
 
@@ -92,11 +104,33 @@ sorted_df = df.sort_values(by=['id_car','time'],ascending=True)
 #
 # print(len_ids)
 
-from numpy import genfromtxt
-# my_data = genfromtxt('my_file.csv', delimiter=',')
-for filename in tqdm((os.listdir('/home/martin/MOBILITY/data/traces'))):
+# from numpy import genfromtxt
+# from numpy.core.defchararray import add
+#
+# # my_data = genfromtxt('my_file.csv', delimiter=',')
+# arr = np.empty(shape=(len(os.listdir('/home/martin/MOBILITY/data/traces')),), dtype=object)
+# for idx, filename in tqdm(enumerate(os.listdir('/home/martin/MOBILITY/data/traces')[:5])):
+#     abs_filename = os.path.join('/home/martin/MOBILITY/data/traces', filename)
+#     filename_parts = abs_filename.split(sep='.')
+#     file_extension = filename_parts[-2]
+#     arr[idx] = genfromtxt(abs_filename, delimiter=',', usecols=(1, 2, 3, 4, 5), encoding=None,dtype=None)
+#     # arr[idx][0] = add(file_extension, arr[idx][0])
+#     arr[idx][0].astype(int)
+#     print(arr[idx].dtype)
+# print(arr)
+# arr = np.empty(hash(5,))
+l = []
+for idx, filename in tqdm(enumerate(os.listdir('/home/martin/MOBILITY/data/traces'))):
     abs_filename = os.path.join('/home/martin/MOBILITY/data/traces', filename)
     filename_parts = abs_filename.split(sep='.')
     file_extension = filename_parts[-2]
-    arr = genfromtxt(abs_filename,dtype=None)
-    print(arr)
+    iterator = roadmaptools.inout.load_csv(abs_filename)
+
+    ls = []
+    for row in iterator:
+        if row[2] == "STREETPICKUP":
+            continue
+
+        ls.append(np.array([int(row[1]),float(row[3]),float(4),row[5]]))
+    l.append(ls)
+
