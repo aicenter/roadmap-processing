2cf65565cbd237d74809e878623d206762e366be
Martin Korytak <korytmar@fel.cvut.cz>
2018-08-07 18:57:40 +0200

    config moved

--- roadmaptools/init.py
+++ roadmaptools/init.py
@@ -4,15 +4,16 @@ from typing import TypeVar
 from typing import Type
 from fconfig.config import Config
 from roadmaptools.config.roadmaptools_config import RoadmaptoolsConfig
-
-C = TypeVar('C', bound=Config)
-CC = TypeVar('CC', bound=Config)
-
-config = RoadmaptoolsConfig()
-
-
-def load_config(client_config: CC, key_in_client: str, client_local_config: str = None,
-                client_config_file_path: str=None):
-    configuration.load(config, client_config, client_config_file_path, client_local_config, key_in_client)
-    return config
+import roadmaptools
+config = roadmaptools.config.roadmaptools_config.config
+# C = TypeVar('C', bound=Config)
+# CC = TypeVar('CC', bound=Config)
+#
+# config = RoadmaptoolsConfig()
+#
+#
+# def load_config(client_config: CC, key_in_client: str, client_local_config: str = None,
+#                 client_config_file_path: str=None):
+#     configuration.load(config, client_config, client_config_file_path, client_local_config, key_in_client)
+#     return config
 
