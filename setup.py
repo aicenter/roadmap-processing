01a13dbce8af2f2c3bb482bb5780e4781f044b75
david <david@147.32.83.252>
2019-01-08 13:51:01 +0100

    dependencies updated

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
 setup(
-   name='roadmaptools',
-   version='1.0.0',
-   description='OSM and geoJSON tools',
-   author='Martin Korytak',
-   author_email='cbudrud@gmail.com',
-   license='MIT',
-   packages=['roadmaptools'],
-   url = 'https://github.com/aicenter/roadmap-processing',
-   download_url = 'https://github.com/aicenter/roadmap-processing/archive/0.2.5.tar.gz',
-   install_requires=['osmread','setuptools','networkx','geojson','pip'],
+    name='roadmaptools',
+    version='1.0.1',
+    description='OSM and geoJSON tools',
+    author='Martin Korytak',
+    author_email='cbudrud@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    url = 'https://github.com/aicenter/roadmap-processing',
+    download_url = 'https://github.com/aicenter/roadmap-processing/archive/0.2.5.tar.gz',
+    install_requires=['osmread','setuptools','networkx>=2.0','geojson', 'gpx_lite', 'rtree', 'tqdm',
+        'typing', 'numpy', 'fconfig', 'googlemaps', 'overpass', 'pandas', 'scipy', 'shapely'],
+    python_requires='>=3',
+    package_data={'roadmaptools.resources': ['*.cfg']}
 )
