aa2b202f4e2b477810b30b4d4b2ddef7c9f1e687
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-09-18 17:02:43 +0200

    release v2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='roadmaptools',
-    version='1.0.2',
+    version='2.0.0',
     description='OSM and geoJSON tools',
     author='Martin Korytak',
     author_email='cbudrud@gmail.com',
@@ -11,8 +11,8 @@ setup(
     packages=setuptools.find_packages(),
     url = 'https://github.com/aicenter/roadmap-processing',
     download_url = 'https://github.com/aicenter/roadmap-processing/archive/0.2.5.tar.gz',
-    install_requires=['osmread','setuptools','networkx>=2.0','geojson', 'gpx_lite', 'rtree', 'tqdm',
-        'typing', 'numpy', 'fconfig', 'googlemaps', 'overpass', 'pandas', 'scipy', 'shapely'],
+    install_requires=['fconfig', 'numpy', 'pandas', 'googlemaps', 'typing', 'gpx_lite', 'tqdm', 'overpass', 'shapely',
+                      'setuptools', 'rtree', 'osmread', 'scipy', 'networkx>=2.0', 'geojson'],
     python_requires='>=3',
     package_data={'roadmaptools.resources': ['*.cfg']}
 )
