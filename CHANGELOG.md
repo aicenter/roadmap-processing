ec67db5f4257184b564a2a3fdedad58f8387e1a1
F.I.D.O <david.fido.fiedler@gmail.com>
2023-09-11 16:59:04 +0200

    new version with removed osmread dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 ## Addded
 - map download from overpass by area name
 
+## Removed
+- the `osmtogeojson` script together with the broken `osmread` dependency
+
 
 # 4.1.0
 
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
     name='roadmaptools',
-    version='4.1.0',
+    version='5.0.0',
     description='Tools for road graph processing',
     author='David Fiedler, Martin Korytak',
     author_email='david.fiedler@agents.fel.cvut.cz',
@@ -30,8 +30,23 @@ setup(
     packages=setuptools.find_packages(),
     url = 'https://github.com/aicenter/roadmap-processing',
     # DO NOT remove the utm packege despite it is not detected by pipreqs
-    install_requires=['fconfig', 'numpy', 'pandas', 'googlemaps', 'typing', 'gpx_lite', 'tqdm', 'overpass', 'shapely',
-                      'setuptools', 'rtree', 'osmread', 'scipy', 'networkx>=2.0', 'geojson', 'utm'],
+    install_requires=[
+        'fconfig',
+        'numpy',
+        'pandas',
+        'googlemaps',
+        'typing',
+        'gpx_lite',
+        'tqdm',
+        'overpass',
+        'shapely',
+        'setuptools',
+        'rtree',
+        'scipy',
+        'networkx>=2.0',
+        'geojson',
+        'utm'
+    ],
     python_requires='>=3',
     package_data={'roadmaptools.resources': ['*.cfg']}
 )
